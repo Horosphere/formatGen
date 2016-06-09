dae375e7cbd4d6809967e810c502550c79de039a
Horosphere <henrywen99@gmail.com>
2016-06-08 21:05:20 -0700

    Initial commit

---
+++ python/formatGen.py
@@ -0,0 +1,25 @@
+import os, sys, vim 
+
+import cpp.includeGuard
+
+def cppFormatIncludeGuardWrapper(rPath):
+    baseDir = os.getcwd()
+    systemRoot = os.path.abspath(os.sep)
+    format = None
+    while baseDir != systemRoot:
+        if os.path.isfile(baseDir + "/.formatGen"):
+            with open(baseDir + "/.formatGen") as f:
+                format = f.readline().strip()
+                break
+        baseDir = os.path.dirname(baseDir)
+    if format == None:
+        vim.command("echoerr \"Unable to find .formatGen file\"")
+        return
+    cpp.includeGuard.format(vim.current.buffer, rPath, format)
+
+def init(commandPy):
+    vim.command('function! s:FGIncludeGuard(rPath)\n' +
+                commandPy + ' cppFormatIncludeGuardWrapper(rPath)\n' +
+                'endfunction')
+    vim.command('command! -nargs=1 FGInclueGuard call s:FGInclueGuard(<f-args>)')
+
