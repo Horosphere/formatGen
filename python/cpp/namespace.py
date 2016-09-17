111ef81ac200833fdb208088eb48acbd35fcaaec
Horosphere <henrywen99@gmail.com>
2016-09-16 21:23:32 -0700

    Move cursor after namespace insertion

--- python/cpp/namespace.py
+++ python/cpp/namespace.py
@@ -4,6 +4,8 @@ def namespace(current, name):
     cursor = current.window.cursor[0]
     buffer = current.buffer
     buffer.append("} // namespace " + name, cursor)
+    buffer.append("", cursor)
     buffer.append("{", cursor)
     buffer.append("namespace " + name, cursor)
+    current.window.cursor = (cursor + 3, current.window.cursor[1])
 
